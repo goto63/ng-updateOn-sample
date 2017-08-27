7efb4376573f7ad3bc52ff5d625f1e2f4283825c
goto63 <tgoto63@gmail.com>
2017-08-27 20:24:05 +0900

    add sample code.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,41 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   template: `
-    <h1>
-      Welcome to {{title}}!!
-    </h1>
+    <form novalidate [formGroup]="myForm">
+      change:<br/>
+      <input formControlName="change"/>
+      <pre class="error">{{ myForm.get('change').errors | json }}</pre>
+      <hr/>
+      blur:<br/>
+      <input formControlName="blur"/>
+      <pre class="error">{{ myForm.get('blur').errors | json }}</pre>
+      <hr/>
+      submit:<br/>
+      <input formControlName="submit"/>
+      <pre class="error">{{ myForm.get('submit').errors | json }}</pre>
+      <hr/>
+      <button>submit</button>
+    </form>
+    values:
+    <pre>{{ myForm.value | json }}</pre>
   `,
-  styles: []
+  styles: [`
+    .error {
+      color: red
+    }
+  `]
 })
 export class AppComponent {
-  title = 'app';
+  myForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.myForm = fb.group({
+      change: ['', {validators: [Validators.required, Validators.minLength(5)], updateOn: 'change'}],
+      blur: ['', {validators: [Validators.required, Validators.minLength(5)], updateOn: 'blur'}],
+      submit: ['', {validators: [Validators.required, Validators.minLength(5)], updateOn: 'submit'}]
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,9 +8,10 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
